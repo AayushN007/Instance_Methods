b640e00afdd692238fee610244fb836db52a9e27
AayushN007 <hemanat2004@gmail.com>
2026-06-18 22:57:09 +0530

    Add files via upload

---
+++ Instance_Method01.py
@@ -0,0 +1,15 @@
+class demo:
+    def __init__(self):
+        self.a = 10
+        self.b = 20
+    def disp(self):
+        x = 100
+        y = 200
+        z = x + y
+        print(z)
+    
+dl = demo()
+print(dl.a)
+print(dl.b)
+
+dl.disp()
---
+++ Instance_Method04.py
@@ -0,0 +1,16 @@
+class demo:
+    def __init__(self):
+        self.a = 10
+        self.b = 20
+    def disp(self, x, y):
+        z = x + y
+        return z
+    
+dl = demo()
+print(dl.a)
+print(dl.b)
+
+n1 = 100
+n2 = 200
+result = dl.disp(n1, n2)
+print(result)
